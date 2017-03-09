02a77e8d4f678728bb5cf778e3a41cb7a3306319
jwaskow <thewasko@gmail.com>
2017-03-09 12:55:33 -0500

    Fix date render in handlebars blogs
    Fixed the createdAt date from displaying in handlebars as a full
    timestamp to only displaying the year month and day it was created at.
    This should reduce confusion for the user.  The ui event processes the
    response and reassigns the new date to the data that gets sent to
    handlebars.
    
    Next Steps: Create forms for blog
    
    Contributors:  J.A.M.S.

--- assets/scripts/blog/ui.js
+++ assets/scripts/blog/ui.js
@@ -3,9 +3,11 @@
 const blogHandlebars = require('../templates/blog-index.handlebars');
 
 const onIndexSuccess = function(data){
-  if (data) {
-  console.log(data);
+for (let i = 0; i < data.blogs.length; i++) {
+  let date = data.blogs[i].createdAt;
+  data.blogs[i].createdAt = date.slice(0, 10);
 }
+
 let blogsIndexHtml = blogHandlebars({ blogs: data.blogs});
 $('#content').html(blogsIndexHtml);
 };
