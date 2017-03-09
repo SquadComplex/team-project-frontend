fe5e4f30655aa0b358aaee7da34bf7bdaef67adb
jwaskow <thewasko@gmail.com>
2017-03-08 19:21:14 -0500

    Add auth folder
    Added boilerplate auth folder with api, events, and ui files.
    
    Next step:  Add related buttons & forms to html with proper ID's.
    Will try to use a bootstrap collapse for auth forms.

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const success = () => {
+  $('#status-box').text('Action Successful');
+};
+
+const failure = () => {
+  $('#status-box').text('Theres seems to have been an error');
+};
+
+const signUpSuccess = () => {
+  $('#status-box').text('Account Created');
+};
+
+const signUpFail = () => {
+  $('#sign-up-message').text('Either the email is not valid or the passwords do not match.');
+};
+
+// On sign in, un-hide game board elements
+const signInSuccess = () => {
+  $('#status-box').text('Sign In Successful');
+
+  $('#signUpButton').addClass('hidden');
+  $('#signInButton').addClass('hidden');
+  $('#changePassButton').removeClass('hidden');
+  $('#sign-out').removeClass('hidden');
+};
+
+const signInFail = () => {
+  $('#sign-in-message').text('The information is incorrect.');
+};
+
+const changePassSuccess = () => {
+  $('#status-box').text('Password Changed Successfully');
+};
+
+const changePassFail = () => {
+  $('#change-pass-message').text('Please fill out the fields.');
+};
+
+// On sign out, hide game board elements
+const signOutSuccess = () => {
+  $('#status-box').text('Sign Out Successful.  Please Sign Up or Sign In to play again.');
+
+  $('#signUpButton').removeClass('hidden');
+  $('#signInButton').removeClass('hidden');
+  $('#changePassButton').addClass('hidden');
+  $('#sign-out').addClass('hidden');
+};
+
+module.exports = {
+  success,
+  failure,
+  signUpSuccess,
+  signUpFail,
+  signInSuccess,
+  signInFail,
+  changePassSuccess,
+  changePassFail,
+  signOutSuccess,
+};
